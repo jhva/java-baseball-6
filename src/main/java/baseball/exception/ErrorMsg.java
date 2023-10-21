27b60b115f241493a6781a96fc716dac2e054798
jhva <junghun8158@naver.com>
2023-10-21 19:18:28 +0900

    :truck: chore: error value name change

--- src/main/java/baseball/exception/ErrorMsg.java
+++ src/main/java/baseball/exception/ErrorMsg.java
@@ -2,6 +2,6 @@ package baseball.exception;
 
 public class ErrorMsg {
 
-    public static final String ERROR_USER_INPUT_SIZE_VALUE = "3자리 숫자를 입력해주세요.";
+    public static final String ERROR_USER_INPUT_SIZE_VALUE = "[ERROR] 3자리 숫자를 입력해주세요.";
 
 }
