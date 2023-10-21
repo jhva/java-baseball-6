97801b19420cceb0693f913663929ba889842478
jhva <junghun8158@naver.com>
2023-10-21 20:07:04 +0900

    :sparkles: feat: add error msg

--- src/main/java/baseball/exception/ErrorMsg.java
+++ src/main/java/baseball/exception/ErrorMsg.java
@@ -3,5 +3,7 @@ package baseball.exception;
 public class ErrorMsg {
 
     public static final String ERROR_USER_INPUT_SIZE_VALUE = "[ERROR] 3자리 숫자를 입력해주세요.";
-
+    public static final String ERROR_USER_INPUT_NOT_NUMBERING_VALUE = "[ERROR] 숫자만 입력해주세요.";
+    public static final String ERROR_USER_INPUT_NOT_RANGE_VALUE = "[ERROR] 1 ~ 9 사이 숫자만 입력해주세요.";
+    public static final String ERROR_USER_INPUT_DUPLICATE_VALUE = "[ERROR] 중복된 숫자를 입력할 수 없습니다.";
 }
