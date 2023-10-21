3b908349c76b3b0de81e2729bc7ddfb5ab5b3db3
jhva <junghun8158@naver.com>
2023-10-21 19:19:09 +0900

    :sparkles: feat: add userInputValueSize check Exception

--- src/main/java/baseball/entity/InputView.java
+++ src/main/java/baseball/entity/InputView.java
@@ -1,7 +1,10 @@
 package baseball.entity;
 
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_SIZE_VALUE;
 import static baseball.utils.MessageUtils.PLEASE_ASK_NUMBER;
+import static baseball.utils.MessageUtils.USER_MAX_SIZE;
 
+import baseball.exception.UserInputSizeException;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
@@ -14,9 +17,20 @@ public class InputView {
         System.out.print(PLEASE_ASK_NUMBER);
     }
 
+    public String printGameContinueFromUserInput() {
+        return Console.readLine();
+    }
+
     public List<Integer> printToInputView() {
         String[] userInputValue = Console.readLine().split("");
+        checkUserInputSize(userInputValue.length);
         return Arrays.stream(userInputValue).mapToInt(Integer::valueOf).
                 boxed().collect(Collectors.toList());
+    }
+
+    public void checkUserInputSize(int userInputLen) {
+        if (userInputLen != USER_MAX_SIZE) {
+            throw new UserInputSizeException(ERROR_USER_INPUT_SIZE_VALUE);
+        }
     }
 }
