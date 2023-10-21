6227cfb76fbe5292d5c1446a66a74b5a8bceeb88
jhva <junghun8158@naver.com>
2023-10-21 22:44:34 +0900

    :sparkles: feat: add userInput validate method

--- src/main/java/baseball/entity/InputView.java
+++ src/main/java/baseball/entity/InputView.java
@@ -1,14 +1,17 @@
 package baseball.entity;
 
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_DUPLICATE_VALUE;
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_NOT_NUMERIC_VALUE;
 import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_SIZE_VALUE;
 import static baseball.utils.MessageUtils.PLEASE_ASK_NUMBER;
 import static baseball.utils.MessageUtils.USER_MAX_SIZE;
 
-import baseball.exception.UserInputSizeException;
+import baseball.exception.UserInputException;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class InputView {
 
@@ -22,15 +25,42 @@ public class InputView {
     }
 
     public List<Integer> printToInputView() {
-        String[] userInputValue = Console.readLine().split("");
-        checkUserInputSize(userInputValue.length);
-        return Arrays.stream(userInputValue).mapToInt(Integer::valueOf).
-                boxed().collect(Collectors.toList());
+        String userInputOnlyNumber = isUserInputNumeric(Console.readLine());
+        checkUserInputSize(userInputOnlyNumber.length());
+        String[] userInputValues = userInputOnlyNumber.split("");
+        return checkDuplicatesAndReturnList(userInputValues);
+    }
+
+    public List<Integer> checkDuplicatesAndReturnList(String[] userInputValues) {
+        Set<Integer> userInputValueSet = new HashSet<>();
+        List<Integer> removeDuplicateAfterValues = new ArrayList<>();
+        int idx = 0;
+        while (idx < USER_MAX_SIZE) {
+            userInputValueSet.add(Integer.parseInt(userInputValues[idx]));
+            removeDuplicateAfterValues.add(Integer.parseInt(userInputValues[idx]));
+            idx++;
+        }
+        checkSetLen(userInputValueSet.size());
+        return removeDuplicateAfterValues;
+    }
+
+    public void checkSetLen(int setLength) {
+        if (setLength < USER_MAX_SIZE) {
+            throw new UserInputException(ERROR_USER_INPUT_DUPLICATE_VALUE);
+        }
     }
 
     public void checkUserInputSize(int userInputLen) {
         if (userInputLen != USER_MAX_SIZE) {
-            throw new UserInputSizeException(ERROR_USER_INPUT_SIZE_VALUE);
+            throw new UserInputException(ERROR_USER_INPUT_SIZE_VALUE);
         }
+    }
+
+    public String isUserInputNumeric(String userInput) {
+        boolean isCheckNumberFrom1to9 = userInput.matches("^[1-9]+$");
+        if (!isCheckNumberFrom1to9) {
+            throw new UserInputException(ERROR_USER_INPUT_NOT_NUMERIC_VALUE);
+        }
+        return userInput;
     }
 }
