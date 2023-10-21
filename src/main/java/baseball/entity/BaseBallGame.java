64466193bbe7e1260a537130b60d60d3dfbf93e6
jhva <junghun8158@naver.com>
2023-10-21 19:14:54 +0900

    :sparkles: feat: add game stop,validationCheckStrike

--- src/main/java/baseball/entity/BaseBallGame.java
+++ src/main/java/baseball/entity/BaseBallGame.java
@@ -1,25 +1,48 @@
 package baseball.entity;
 
+import static baseball.utils.MessageUtils.CHECK_GAME_EXIT_NUMBER;
+import static baseball.utils.MessageUtils.CHECK_STRIKE_MAX;
 import static baseball.utils.MessageUtils.GAME_START;
 
 import baseball.utils.MessageUtils;
+import java.util.Objects;
 
 public class BaseBallGame extends Computer {
 
+    private static boolean hasGame = true;
 
     public void start(InputView inputView) {
         System.out.println(GAME_START);
-        while (true) {
-            randomGenerator();
+        randomGenerator();
+        while (hasGame) {
             inputView.printFromUserView();
-            int[] s = userMatchesComputerRandom(inputView.printToInputView());
-            MessageUtils.commonFormatter(s);
+            int[] userScore = userMatchesComputerRandom(inputView.printToInputView());
+            int strike = userScore[1];
+            MessageUtils.commonFormatter(userScore);
+            checkForStrike(inputView, strike);
         }
     }
 
-    public void stop() {
+    public void checkForStrike(InputView inputView, int strike) {
+        if (strike == CHECK_STRIKE_MAX) {
+            boolean hasThreeStrike = MessageUtils.hasCurrentThreeStrike(strike);
+            stop(inputView, hasThreeStrike);
+        }
     }
 
-    public void reset() {
+    public void stop(InputView inputView, boolean hasThreeStrike) {
+        if (hasThreeStrike) {
+            validateGameControllerValue(inputView.printGameContinueFromUserInput());
+        }
+    }
+
+
+    public void validateGameControllerValue(String userInput1or2) {
+        if (Objects.equals(userInput1or2, CHECK_GAME_EXIT_NUMBER)) {
+            hasGame = false;
+        } else {
+            randomGenerator();
+            
+        }
     }
 }
