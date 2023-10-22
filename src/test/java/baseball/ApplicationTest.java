2fcdb6a5c4cc9b94d4f09d0ef37ba29475d3d4df
jhva <junghun8158@naver.com>
2023-10-22 23:24:31 +0900

    :recycle: refactor: magicNumber modify for ApplicationTestException

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_DUPLICATE_VALUE;
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_NOT_NUMERIC_VALUE;
+import static baseball.exception.ErrorMsg.ERROR_USER_INPUT_SIZE_VALUE;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,17 +34,34 @@ class ApplicationTest extends NsTest {
 
     @Test
     void 사용자가_입력한_값_중복체크_테스트() {
+
+        String duplicateNumber = "122";
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("122"))
+                assertThatThrownBy(() -> runException(duplicateNumber))
                         .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ERROR_USER_INPUT_DUPLICATE_VALUE)
         );
     }
 
     @Test
     void 사용자가_0또는_한글을_입력했을경우_예외_테스트() {
+        String korean = "한글";
+        String numeric0 = "0";
+        String english = "english";
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("0", "한글", "english"))
+                assertThatThrownBy(() -> runException(numeric0, korean, english))
                         .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(ERROR_USER_INPUT_NOT_NUMERIC_VALUE)
+        );
+    }
+
+    @Test
+    void 사용자_입력_길이_값이_3이하인_경우_예외_테스트() {
+        String userInputValue = "12";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(userInputValue))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContainingAll(ERROR_USER_INPUT_SIZE_VALUE)
         );
     }
 
